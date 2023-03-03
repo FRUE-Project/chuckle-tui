0bc867e5a38f0e83a5d57bc5b6bab2324bd3c27a
Tuhkis <nicolindstromj@gmail.com>
2023-03-03 21:06:28 +0200

    Start adding keyboard navigation

--- chuckle-tui.py
+++ chuckle-tui.py
@@ -2,10 +2,18 @@ import sys
 import curses
 from enum import Enum
 
+MENU_WIDTH = 40
+
 class Opts(Enum):
+    # Main menu
     CONTINUE_INSTALL  = 1
     TERMINATE_INSTALL = 2
     SET_LANGUAGE      = 3
+
+    # Set language menu
+    # NOTE Language select menu needs a rewrite to scan 'n stuff to detect locales 'n shit
+    KB_EN_US = 4
+    KB_FI    = 5
 
     def opt_to_text(opt):
         if opt == Opts.CONTINUE_INSTALL:
@@ -14,36 +22,42 @@ class Opts(Enum):
             return 'Stop installing'
         elif opt == Opts.SET_LANGUAGE:
             return 'Set install language'
+        elif opt == Opts.KB_EN_US:
+            return 'English (US)'
+        elif opt == Opts.KB_FI:
+            return 'Finnish'
 
-    def opt_to_action(opt):
+    def opt_to_action(opt, menus):
         if opt == Opts.CONTINUE_INSTALL:
             pass
         elif opt == Opts.TERMINATE_INSTALL:
             sys.exit()
         elif opt == Opts.SET_LANGUAGE:
-            pass
+            menus.append(Menu([Opts.KB_EN_US, Opts.KB_FI], MENU_WIDTH+1, 0))
 
 class Menu:
-    def __init__(self, options):
+    def __init__(self, options, x=0, y=0):
         self.options = options
         self.chosen = 0
-        self.w = 35
+        self.w = MENU_WIDTH
+        self.x = x
+        self.y = y
 
     def draw_borders(self, stdscr):
-        stdscr.addstr(0, 0, "╔")
+        stdscr.addstr(self.y, self.x, "╔")
         for i in range(self.w - 1):
-            stdscr.addstr(0, i + 1, "═")
-        stdscr.addstr(0, self.w, "╗")
+            stdscr.addstr(self.y, i+self.x + 1, "═")
+        stdscr.addstr(self.y, self.x+self.w, "╗")
 
         for i in range(len(self.options)):
-            stdscr.addstr(i + 1, 0, "║")
+            stdscr.addstr(self.y + i + 1, self.x, "║")
         for i in range(len(self.options)):
-            stdscr.addstr(i + 1, self.w, "║")
+            stdscr.addstr(i + 1 + self.y, self.w + self.x, "║")
 
-        stdscr.addstr(len(self.options) + 1, 0, "╚")
+        stdscr.addstr(len(self.options) + 1 + self.y, self.x, "╚")
         for i in range(self.w - 1):
-            stdscr.addstr(len(self.options) + 1, i + 1, "═")
-        stdscr.addstr(len(self.options) + 1, self.w, "╝")
+            stdscr.addstr(len(self.options) + 1 + self.y, i + 1 + self.x, "═")
+        stdscr.addstr(len(self.options) + 1 + self.y, self.w + self.x, "╝")
 
     def display(self, stdscr):
         self.draw_borders(stdscr)
@@ -51,34 +65,40 @@ class Menu:
         r = 0
         for o in self.options:
             if o.value == self.options[self.chosen].value:
-                stdscr.addstr(r + 1, 2, '* ' + Opts.opt_to_text(o))
+                stdscr.addstr(r + 1 + self.y, 2 + self.x, '* ' + Opts.opt_to_text(o))
             else:
-                stdscr.addstr(r + 1, 4, Opts.opt_to_text(o))
+                stdscr.addstr(r + 1 + self.y, 4 + self.x, Opts.opt_to_text(o))
             r += 1
 
-    def choose_current(self):
-        Opts.opt_to_action(self.options[self.chosen])
+    def choose_current(self, menus):
+        Opts.opt_to_action(self.options[self.chosen], menus)
 
 def main(stdscr):
     stdscr.keypad(True)
     curses.curs_set(False)
-    main_menu = Menu([
+    menus = [Menu([
         Opts.SET_LANGUAGE,
         Opts.CONTINUE_INSTALL,
         Opts.TERMINATE_INSTALL
-    ])
+    ], 0, 0)]
+    chosen = 0
     while True:
         curses.noecho()
         stdscr.clear()
-        main_menu.display(stdscr)
+        for m in menus:
+            m.display(stdscr)
         stdscr.refresh()
         i = stdscr.getch()
         if i == curses.KEY_DOWN:
-            main_menu.chosen += 1
-        if i == curses.KEY_UP:
-            main_menu.chosen -= 1
-        if i == curses.KEY_ENTER:
-            main_menu.choose_current()
+            menus[chosen].chosen += 1
+        elif i == curses.KEY_UP:
+            menus[chosen].chosen -= 1
+        elif i == curses.KEY_RIGHT and chosen == 0:
+            menus[0].choose_current(menus)
+            chosen = 1
+        elif i == curses.KEY_LEFT and chosen == 1:
+            menus.remove(menus[1])
+            chosen = 0
 
 curses.wrapper(main)
 
